493d344cf7e5580f52b0e359a0b3dafee338c382
David.Ou <gault_1@icloud.com>
2020-05-20 10:58:42 +0900

    Remove plugin setting page because project progress is always indicated.

--- init.rb
+++ init.rb
@@ -1,22 +1,16 @@
-require 'redmine'
+require "redmine"
+require "projects_tree_view_projects_helper_patch"
 
-require 'projects_tree_view_projects_helper_patch'
-
-# Patches to the Redmine core.
 Rails.configuration.to_prepare do
-  require_dependency 'projects_helper'
+  require_dependency "projects_helper"
   ProjectsHelper.send(:include, ProjectsTreeView::ProjectsHelperPatch)
 end
 
 Redmine::Plugin.register :projects_tree_view do
-  name 'Projects Tree View plugin'
-  author 'Chris Peterson and Github community'
-  description 'This is a Redmine plugin which will turn the projects page into a tree view'
-  url 'https://github.com/cforce/projects_tree_view'
-  version '0.0.9'
-  requires_redmine :version_or_higher => '3.0.0'
-
-  settings :default => {
-    'show_project_progress' => true
-  }, :partial => 'settings/project_tree_settings'
+  name             "Projects Tree View plugin"
+  author           "Chris Peterson and Github community"
+  description      "This is a Redmine plugin which will turn the projects page into a tree view"
+  url              "https://github.com/cforce/projects_tree_view"
+  version          "0.0.9_f1"
+  requires_redmine :version_or_higher => "4.1.0"
 end
